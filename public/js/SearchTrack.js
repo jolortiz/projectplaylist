bd5c9291098225a5c0961148db20b73d818779f8
jolortiz <jolortiz@ucsc.edu>
2018-01-31 11:42:23 -0800

    search bar

---
+++ public/js/SearchTrack.js
@@ -0,0 +1,68 @@
+/*
+    SearchTrack.js
+
+    ~ uses Select2 to search the spotify library and return results
+
+*/
+
+console.log(global_token);
+
+
+$(".js-data-example-ajax").select2({
+    placeholder: "Please click to search for a track.", 
+    ajax: {
+        url: "https://api.spotify.com/v1/search",
+        dataType: 'json',
+        delay: 250,
+        headers: {
+            "Authorization": "Bearer " + global_token, 
+            "Content-Type": "application/json",
+        }, 
+        data: function (params) {
+            return {
+                query: params.term, 
+                type: "track", 
+                market: "US", 
+                limit: 10
+            };
+        }, 
+        processResults: function (data) {
+            var items = [];
+            data.tracks.items.forEach(function(d) {
+                //if (selectedTrack.indexOf(d.id) == -1) {
+                    items.push(d);
+                //}
+            });
+            
+            return {
+                results: items
+            };
+        }, 
+        cache: true
+    },
+    escapeMarkup: function (markup) { return markup; },
+    minimumInputLength: 1,
+    templateResult: formatTrack,
+    templateSelection: formatTrackSelection
+});
+
+function formatTrack(track) {
+    if (track.loading) {
+        return "<div>Searching for tracks..</div>";
+    }
+    
+    var html = "<div class='select2-result-repository clearfix'>" +
+    "<div class='select2-result-repository__avatar'><img src='" +  ((track.album.images.length > 2) ? track.album.images[2].url : '../assets/unknown.png') + "' /></div>" +
+    "<div class='select2-result-repository__meta'>" +
+      "<div class='select2-result-repository__title'>" + track.name + " -<br/>" + track.album.artists[0].name + "</div></div></div>";
+    
+    return html;
+}
+
+function formatTrackSelection(track) {
+    if (track.id == "") {
+        return "Click to search for a track.";
+    } else {
+        return "Last selected: " + track.name + " - " + (track.album.artists[0].name);
+    }
+}
