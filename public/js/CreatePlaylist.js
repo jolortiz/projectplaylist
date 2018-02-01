8f30da1a5612a6069147fd78ed3c20a10f04cd6b
Merrick Swaffar <merrickswaffar@Merricks-MacBook.local>
2018-02-01 09:11:01 -0800

    Creating a playlist(doesn't work)

---
+++ public/js/CreatePlaylist.js
@@ -0,0 +1,27 @@
+console.log(global_token);
+
+function createPlaylist(username, playlistName, isPublic){
+	var urlString = 'https://api.spotify.com/v1/users/' + username + '/playlists';
+
+	var jsonData = {
+	"name": playlistName,
+	"public": isPublic
+	};
+
+	$.ajax({
+	type: 'POST',
+	url: urlString,
+	data: jsonData,
+	dataType: 'json',
+	headers: {
+	'Authorization': 'Bearer ' + global_token
+	},
+	contentType: 'application/json',
+	success: function(result) {
+	console.log('Success');
+	},
+	error: function() {
+	console.log('Error');
+	}
+	});
+}
