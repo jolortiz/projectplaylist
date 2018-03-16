9f38f00fa6af4688d1f5611de3b0fca6f46e2584
Alec McLeod <aimcleod@ucsc.edu>
2018-03-16 11:53:38 -0700

    changed socketevents connection

--- public/js/SocketEvents.js
+++ public/js/SocketEvents.js
@@ -2,10 +2,11 @@
 var socket = io.connect('http://musaicplaylist.heroku.com');
 
 socket.on('connect', function (playlist) {
+    console.log("Here too");
     socket.emit('join', id);
 });
 
 socket.on('refresh tracks', function(data) {
-    //console.log("Heerio");
+    console.log("Heerio");
     location.reload();
 });
